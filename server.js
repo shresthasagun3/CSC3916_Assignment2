06198da5e422dad8e9a6bb8e85670b4d8b953d20
shresthasagun3 <ssagun336@gmail.com>
2026-03-01 13:15:39 -0700

    fixing comment

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ router.route('/movies')
         res.json(o)
     })
     .put((req, res) => {
-        / HTTP PUT Method
+        // HTTP PUT Method
         // Requires JWT authentication.
         // Returns a JSON object with status, message, headers, query, and env.
         var o = getJSONObjectForMovieRequirement(req);
